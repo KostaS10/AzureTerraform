1a5c2cb7b68aab064b5d4e570144cb26382442b3
Kosta <kosta.sotic@gmail.com>
2022-04-23 13:52:15 +0000

    provider changes

--- providers.tf
+++ providers.tf
@@ -6,10 +6,17 @@ terraform {
         version = "=3.3.0"
     }
   }
+  backend "azurerm"{
+	resource_group_name = "terraform"
+	storage_account_name = "terraformsgkosta"
+	container_name = "tfstate"
+	key = "terraform.tfstate"
+}
 }
 
 provider "azurerm" {
     features {}
+    use_microsoft_graph = true
     subscription_id = var.subscription_id
     client_id = var.client_id
     client_secret = var.client_secret
