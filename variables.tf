a24b17771c9d005f578bf105e403cccddd65b812
Kosta S <kosta.sotic@gmail.com>
2022-04-23 15:56:10 +0200

    resource group creation

--- variables.tf
+++ variables.tf
@@ -2,18 +2,23 @@ variable subscription_id {
   type        = string
   default     = ""
 }
-
 variable client_id {
   type        = string
   default     = ""
 }
-
 variable client_secret {
   type        = string
   default     = ""
 }
-
 variable tenant_id {
   type        = string
   default     = ""
+}
+variable rgName {
+  type        = string
+  default     = ""
+}
+variable location {
+  type        = string
+  default     = "west europe"
 }
