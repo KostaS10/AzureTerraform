c753f4dbd20c4254a93f4ca9fadd91968a90cade
Kosta <kosta.sotic@gmail.com>
2022-04-23 14:03:35 +0000

    format

--- providers.tf
+++ providers.tf
@@ -2,23 +2,23 @@ terraform {
   required_version = "=1.1.9"
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "=3.3.0"
+      source  = "hashicorp/azurerm"
+      version = "=3.3.0"
     }
   }
-  backend "azurerm"{
-    use_microsoft_graph = true
-	resource_group_name = "terraform"
-	storage_account_name = "terraformsgkosta"
-	container_name = "tfstate"
-	key = "terraform.tfstate"
-}
+  backend "azurerm" {
+    use_microsoft_graph  = true
+    resource_group_name  = "terraform"
+    storage_account_name = "terraformsgkosta"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
-    features {}
-    subscription_id = var.subscription_id
-    client_id = var.client_id
-    client_secret = var.client_secret
-    tenant_id = var.tenant_id
+  features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
--- variables.tf
+++ variables.tf
@@ -1,24 +1,24 @@
-variable subscription_id {
-  type        = string
-  default     = ""
+variable "subscription_id" {
+  type    = string
+  default = ""
 }
-variable client_id {
-  type        = string
-  default     = ""
+variable "client_id" {
+  type    = string
+  default = ""
 }
-variable client_secret {
-  type        = string
-  default     = ""
+variable "client_secret" {
+  type    = string
+  default = ""
 }
-variable tenant_id {
-  type        = string
-  default     = ""
+variable "tenant_id" {
+  type    = string
+  default = ""
 }
-variable rgName {
-  type        = string
-  default     = ""
+variable "rgName" {
+  type    = string
+  default = ""
 }
-variable location {
-  type        = string
-  default     = "west europe"
+variable "location" {
+  type    = string
+  default = "west europe"
 }
