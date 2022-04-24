e1e1b44fe39fc3b65892edd4ebf8be949d54090b
Kosta S <kosta.sotic@gmail.com>
2022-04-24 10:34:39 +0200

    adding soft delete

--- main.tf
+++ main.tf
@@ -11,4 +11,5 @@ resource "azurerm_key_vault" "kv" {
   resource_group_name = azurerm_resource_group.rg.name
   tenant_id = data.azurerm_client_config.current.tenant_id
   purge_protection_enabled = true
+  soft_delete_retention_days  = 7
 }
