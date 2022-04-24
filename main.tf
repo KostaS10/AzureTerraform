c9e139cdbdbc9480968088a718719133077935ba
Kosta S <kosta.sotic@gmail.com>
2022-04-24 10:28:28 +0200

    test kv

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_resource_group" "rg" {
   name     = var.rgName
   location = var.location
+}
+
+resource "azurerm_key_vault" "kv" {
+  name = var.kvName
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  purge_protection_enabled = false
 }
